a84bb2f53d90078fe30f36fd12119e843ee3528f
hyeongsh <shinhs6384@naver.com>
2025-12-31 17:43:20 +0900

    flyway 재변경

--- notify/src/main/resources/db/migration/V3__change_logic.sql
+++ notify/src/main/resources/db/migration/V3__change_logic.sql
@@ -1,8 +1,8 @@
 -- 다른 테이블 삭제 코드 추가
 
-DROP TABLE notification_channels;
-DROP TABLE notification_templates;
-DROP TABLE notifications;
+DROP TABLE IF EXISTS notification_channels;
+DROP TABLE IF EXISTS notification_templates;
+DROP TABLE IF EXISTS notifications;
 
 CREATE TABLE fcm_tokens (
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
@@ -10,6 +10,6 @@ CREATE TABLE fcm_tokens (
     token VARCHAR(100) NOT NULL,
     device_type VARCHAR(20) NOT NULL,
     browser_type VARCHAR(20) NOT NULL,
-    updated_at DATETIME NOT NULL,
+    updated_at DATE NOT NULL,
     enabled BOOLEAN DEFAULT TRUE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
