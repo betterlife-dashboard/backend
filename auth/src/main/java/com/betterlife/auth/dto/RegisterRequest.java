84e58d077f0920b8a9c2d5f3de11318bb233ea05
hyeongsh <shinhs6384@naver.com>
2026-01-12 23:34:43 +0900

    auth/register api swagger 문서 예시 추가

--- auth/src/main/java/com/betterlife/auth/dto/RegisterRequest.java
+++ auth/src/main/java/com/betterlife/auth/dto/RegisterRequest.java
@@ -1,5 +1,6 @@
 package com.betterlife.auth.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -10,12 +11,15 @@ import lombok.*;
 @AllArgsConstructor
 public class RegisterRequest {
 
+    @Schema(example = "test")
     @NotBlank
     private String name;
 
+    @Schema(example = "sample@test.com")
     @NotBlank @Email
     private String email;
 
+    @Schema(example = "AbCd1234")
     @NotBlank @Size(min=8, max=72)
     private String password;
 }
